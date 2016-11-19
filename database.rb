d517de435af7b0dbb0d35074a0c335a7f0244d64
Hamilton Clark <dhclark2@ncsu.edu>
2016-11-18 19:16:52 -0500

    Program works.  Next step: format output to look better.

--- database.rb
+++ database.rb
@@ -5,28 +5,48 @@ class Employee
 end
 
 class Database
+  # Dear fellow programmer
+  #
+  # This initializer (method) creates a new people array
+  #
+  # Then it opens a CSV file, reading each line
+  # pulls out the values and creates a new person with those details
+  # adding them to the array
   def initialize
     @people = []
 
-    CSV.foreach("employees.csv", headers:true) do |person|
-      name = person["name"]
-      phone = person["phone"]
-      address = person["address"]
-      position = person["position"]
-      salary = person["salary"]
-      slack = person["slack"]
-      github = person["github"]
+    # Ruby, please open a CSV file for me, that file will have headers on first row
+    # for every row in the CSV file, give me that data in a variable called "csv"
+    CSV.foreach("employees.csv", headers:true) do |csv|
+      # make a new employee object for me, and put it in a variable called employee
+      employee = Employee.new
 
-      add_new_employee(name, phone, address, position, salary, slack, github)
+      # Set the employee object's name to the value of the CSV column "name" (which we can find through the person csv)
+
+      employee.name = csv["name"]
+      employee.phone = csv["phone"]
+      employee.address = csv["address"]
+      employee.position = csv["position"]
+      employee.salary = csv["salary"]
+      employee.slack = csv["slack"]
+      employee.github = csv["github"]
+
+      @people << employee
+
     end
   end
 
   def prompt_user
     puts "Hello.  Would you like to Add (A), Search (S), or Delete (D) an entry?"
+    puts "To see a fully list of the database  (F)  "
     gets.chomp
   end
 
-  def add_new_employee
+  # Dear fellow programmer
+  #
+  # This method promps a human for details about a new person
+  # and then adds them to the array
+  def add_new_person
     employee = Employee.new
 
     puts "What is the name?"
@@ -43,15 +63,41 @@ class Database
     employee.slack = gets.chomp
     puts "What is the GitHub account?"
     employee.github = gets.chomp
+    puts
+    puts "Thank you.  These records have been added to the database."
+    puts
 
     @people << employee
   end
 
-  # def search_and_print(name)
-  #
-  # end
+  def search_and_print
+    puts "What is the name?"
+    name = gets.chomp
+
+    @people.each do |person|
+      if person.name == name
+        puts
+        puts "Employee: #{person.name}"
+        puts "Phone Numer: #{person.phone}"
+        puts "Home Address: #{person.address}"
+        puts "Job Title: #{person.position}"
+        puts "Annual Salary: #{person.salary}"
+        puts "Slack Account: #{person.slack}"
+        puts "GitHub Account: #{person.github}"
+        puts
+        puts
+      else
+        puts
+        puts "No Match"
+        break
+      end
+      break
+    end
+
+  end
 
   def print_all_results
+
     @people.each do |person|
       puts
       puts "Employee: #{person.name}"
@@ -66,8 +112,16 @@ class Database
     end
   end
 
-  def delete_employee_entry
-    asdf
+  def delete_who?
+    puts "What is the name?"
+    name = gets.chomp
+
+    if @people.any? { |person| person.name == name }
+      @people.delete_if { |person| person.name == name }
+
+    else
+      puts "Nobody matches"
+    end
   end
 
   def save_entry
@@ -92,19 +146,24 @@ class Database
 
       loop do
         if prompt_answer == "A"
-          add_new_employee
+          add_new_person
           save_entry
           break
         end
 
         if prompt_answer == "S"
-          # search_and_print
+          search_and_print
+          break
+        end
+
+        if prompt_answer == "F"
           print_all_results
           break
         end
 
         if prompt_answer == "D"
-          delete_employee_entry
+          delete_who?
+          save_entry
           break
         end
 
