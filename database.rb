9cb8a121da4c431a0c71a6686c27c4515fb58ddc
Hamilton Clark <dhclark2@ncsu.edu>
2016-11-22 15:13:08 -0500

    Add files via upload

--- database.rb
+++ database.rb
@@ -47,77 +47,82 @@ class Database
   # This method promps a human for details about a new person
   # and then adds them to the array
   def add_new_person
-    employee = Employee.new
 
-    puts "What is the name?"
-    employee.name = gets.chomp
-    puts "What is the phone number?"
-    employee.phone = gets.chomp
-    puts "What is the address?"
-    employee.address = gets.chomp
-    puts "What is the position?"
-    employee.position = gets.chomp
-    puts "What is the salary?"
-    employee.salary = gets.chomp
-    puts "What is the slack account?"
-    employee.slack = gets.chomp
-    puts "What is the GitHub account?"
-    employee.github = gets.chomp
-    puts
-    puts "Thank you.  These records have been added to the database."
-    puts
-
-    @people << employee
-  end
-
-  def search_and_print
     puts "What is the name?"
     name = gets.chomp
 
-    @people.each do |person|
-      if person.name == name
+    if @people.any? { |employee| employee.name == name }
+      puts "This person is already in the database."
+    else
+      employee = Employee.new
+      employee.name = name
+      puts "What is the phone number?"
+      employee.phone = gets.chomp
+      puts "What is the address?"
+      employee.address = gets.chomp
+      puts "What is the position?"
+      employee.position = gets.chomp
+      puts "What is the salary?"
+      employee.salary = gets.chomp
+      puts "What is the slack account?"
+      employee.slack = gets.chomp
+      puts "What is the GitHub account?"
+      employee.github = gets.chomp
+      puts
+      puts "Thank you.  These records have been added to the database."
+      puts
+
+      @people << employee
+    end
+  end
+
+  def search_and_print
+    puts "What is the name? Search can also be the exact Github or Slack account name."
+    name = gets.chomp
+
+    matching_term = @people.select { |employee| employee.name == name || employee.slack == name || employee.github == name }
+
+    if matching_term.empty?
+      puts "The search returned no such account."
+    else
+      matching_term.each do |employee|
         puts
-        print "Employee:...........| #{person.name}"
+        print "Employee:...........| #{employee.name}"
         puts
-        print "Phone Numer:........| #{person.phone}"
+        print "Phone Numer:........| #{employee.phone}"
         puts
-        print "Home Address:.......| #{person.address}"
+        print "Home Address:.......| #{employee.address}"
         puts
-        print "Job Title:..........| #{person.position}"
+        print "Job Title:..........| #{employee.position}"
         puts
-        print "Annual Salary:......| #{person.salary}"
+        print "Annual Salary:......| #{employee.salary}"
         puts
-        print "Slack Account:......| #{person.slack}"
+        print "Slack Account:......| #{employee.slack}"
         puts
-        print "GitHub Account:.....| #{person.github}"
+        print "GitHub Account:.....| #{employee.github}"
         puts
-      else
-        puts
-        puts "No Match"
-        break
       end
-      break
     end
 
   end
 
   def print_all_results
 
-    @people.each do |person|
+    @people.each do |employee|
       puts
-      print "Employee:...........| #{person.name}"
+      print "Employee:...........| #{employee.name}"
       puts
-      print "Phone Numer:........| #{person.phone}"
+      print "Phone Numer:........| #{employee.phone}"
       puts
-      print "Home Address:.......| #{person.address}"
+      print "Home Address:.......| #{employee.address}"
       puts
-      print "Job Title:..........| #{person.position}"
+      print "Job Title:..........| #{employee.position}"
       puts
-      print "Annual Salary:......| #{person.salary}"
+      print "Annual Salary:......| #{employee.salary}"
       puts
-      print "Slack Account:......| #{person.slack}"
+      print "Slack Account:......| #{employee.slack}"
       puts
-      print "GitHub Account:.....| #{person.github}"
+      print "GitHub Account:.....| #{employee.github}"
       puts
       puts
     end
@@ -127,8 +132,8 @@ class Database
     puts "What is the name?"
     name = gets.chomp
 
-    if @people.any? { |person| person.name == name }
-      @people.delete_if { |person| person.name == name }
+    if @people.any? { |employee| employee.name == name }
+      @people.delete_if { |employee| employee.name == name }
 
     else
       puts "Nobody matches"
@@ -139,8 +144,8 @@ class Database
     csv = CSV.open("employees.csv", "w")
     csv.add_row %w[name phone address position salary slack github]
 
-    @people.each do |person|
-      csv.add_row [person.name, person.phone, person.address, person.position, person.salary, person.slack, person.github]
+    @people.each do |employee|
+      csv.add_row [employee.name, employee.phone, employee.address, employee.position, employee.salary, employee.slack, employee.github]
     end
     csv.close
   end
